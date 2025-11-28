103860721f62e3851004e3669ff01a8f404076a2
Mateus Freitas <mateusnascimentodf@gmail.com>
2025-11-28 10:24:52 +0000

    Adiciona página em construção

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,35 @@
+import React from "react";
+
 function Home() {
-  return <h1>Vamos voar com o curso!!!</h1>;
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        textAlign: "center",
+        padding: "16px",
+      }}
+    >
+      <div>
+        <h1
+          style={{
+            fontSize: "2.25rem",
+            fontWeight: "bold",
+            marginBottom: "16px",
+          }}
+        >
+          🚧 Em construção
+        </h1>
+        <p style={{ fontSize: "1.125rem", maxWidth: "800px", color: "#666" }}>
+          Uma plataforma colaborativa para estudantes e pesquisadores
+          compartilharem conhecimento, debaterem ideias e evoluírem
+          academicamente.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
